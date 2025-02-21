cdbec437f0f4755c3c49d4a578d5cb22d56676b3
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-21 18:14:23 +0200

    Q3

---
+++ Assignment/FixedSizeList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class FixedSizeList<T>
+    {
+        private T[] items; // مصفوفة لتخزين العناصر
+        private int count; // عدد العناصر الحالية في القائمة
+        private int capacity; // السعة القصوى للقائمة
+
+        // Constructor
+        public FixedSizeList(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than 0.");
+            }
+
+            this.capacity = capacity;
+            items = new T[capacity];
+            count = 0;
+        }
+
+        // Method to add an item to the list
+        public void Add(T item)
+        {
+            if (count >= capacity)
+            {
+                throw new InvalidOperationException("Cannot add more items. The list is full.");
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        // Method to get an item at a specific index
+        public T Get(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException($"Index {index} is out of range. Valid range is 0 to {count - 1}.");
+            }
+
+            return items[index];
+        }
+
+        // Property to get the current count of items
+        public int Count
+        {
+            get { return count; }
+        }
+
+        // Property to get the capacity of the list
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -33,7 +33,7 @@ namespace Assignment
             {
                 if (number % 2 == 0)
                 {
-                    evenNumbers.Add(number); 
+                    evenNumbers.Add(number);
                 }
             }
 
@@ -41,28 +41,77 @@ namespace Assignment
         }
 
         static void Main(string[] args)
-            {
-                #region Q1
-                //ArrayList List = new ArrayList { 1, 2, 3, 4, 5 };
+        {
+            #region Q1
+            //ArrayList List = new ArrayList { 1, 2, 3, 4, 5 };
 
-                //ReverseArrayList(List);
+            //ReverseArrayList(List);
 
-              
-                //foreach (var item in List)
-                //{
-                //    Console.Write(item + " ");
-                //}
+
+            //foreach (var item in List)
+            //{
+            //    Console.Write(item + " ");
+            //}
 
             #endregion
 
             #region Q2
 
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            List<int> evenNumbers = GetEvenNumbers(numbers);
+            //List<int> evenNumbers = GetEvenNumbers(numbers);
 
-            Console.WriteLine(string.Join(" ", evenNumbers));
+            //Console.WriteLine(string.Join(" ", evenNumbers));
+            #endregion
+
+            #region Q3
+
+            //try
+            //{
+
+            //    Console.Write("Enter the capacity of the list: ");
+            //    int capacity = int.Parse(Console.ReadLine());
+
+
+
+            //    FixedSizeList<int> list = new FixedSizeList<int>(capacity);
+
+
+            //    while (true)
+            //    {
+            //        Console.Write("Enter an item to add (or type 'done' to finish): ");
+            //        string input = Console.ReadLine();
+
+            //        if (input.ToLower() == "done")
+            //        {
+            //            break;
+            //        }
+
+            //        int item = int.Parse(input);
+            //        list.Add(item);
+            //    }
+
+
+
+            //    Console.WriteLine("\nItems in the list:");
+            //    for (int i = 0; i < list.Count; i++)
+            //    {
+            //        Console.WriteLine($"Index {i}: {list.Get(i)}");
+            //    }
+
+
+            //    Console.Write("\nEnter an index to retrieve an item: ");
+            //    int index = int.Parse(Console.ReadLine());
+            //    Console.WriteLine($"Item at index {index}: {list.Get(index)}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+
+
             #endregion
         }
-        }
     }
+}       
+  
