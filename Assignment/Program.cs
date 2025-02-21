8b07822b0396be5b15d362f44a6e319ab86e17c4
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-02-21 16:41:58 +0200

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace Assignment
+{
+    internal class Program
+    {
+
+
+        static void ReverseArrayList(ArrayList list)
+        {
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start < end)
+            {
+
+                object temp = list[start];
+                list[start] = list[end];
+                list[end] = temp;
+
+
+                start++;
+                end--;
+            }
+        }
+            static void Main(string[] args)
+            {
+                #region Q1
+                ArrayList List = new ArrayList { 1, 2, 3, 4, 5 };
+
+                ReverseArrayList(List);
+
+              
+                foreach (var item in List)
+                {
+                    Console.Write(item + " ");
+                }
+
+                #endregion
+            }
+        }
+    }
